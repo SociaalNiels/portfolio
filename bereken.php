f7cadc04574e28326d24f20961c52edd8ece1ad4
SociaalNiels <70744600+SociaalNiels@users.noreply.github.com>
2021-01-21 21:17:09 +0100

    Add files via upload

---
+++ bereken.php
@@ -0,0 +1,8 @@
+<?php
+
+$schepen = array(
+    "athens glory" => ["naam" => "athens glory", "max_lading" => 10000, "m3" => 100],
+    "apostolos II" => ["naam" => "apostolos II", "max_lading" => 20000, "m3" => 200],
+    "hmm le havre" => ["naam" => "hmm le havre", "max_lading" => 30000, "m3" => 300],
+)
+?>
